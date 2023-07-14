fc30701be89d02633b05561219cdaa9c20561b1b
ngrabbs <ngrabbs@gmail.com>
2023-07-14 08:57:21 -0500

    update controller

--- testStandController.ino
+++ testStandController.ino
@@ -1,14 +1,50 @@
 #include <SPI.h>
 
+// constants
+const int fuelButtonPin = 53;     // the pin for the physical test stand fuel button
+const int fuelSolenoidPin =  37;  // the pin that fires the relay connected to the fuel
+                                  // solenoid
+const int oxidizerSolenoidPin = 0; // the pin that fires the relay connected to the 
+                                   // oxidizer solenoid
+const int armPin = 35;             // the master enable physical switch
+const int enableRelayPin = 39;     // power up the relay board ( might not need )
+const int igniterButtonPin = 0;    // igniter physical button pin
+const int igniterSolenoidPin = 0;  // the pin that fires the relay connected to the igniter
+                                   // solenoid
+
+// variables
+int fuelButtonState = 0;
+int oxidizerButtonState = 0;
+int igniterButtonState = 0;
+
+int fuelOUTPUT = 0;
+int oxidizerOUTPUT = 0;
+int igniterOUTPUT = 0;
+
 void setup() {
+  // SPI setup
   // have to send on master in, *slave out*
   pinMode(MISO, OUTPUT);
-
   // turn on SPI in slave mode
   SPCR |= _BV(SPE);
-
   // turn on interrupts
   SPI.attachInterrupt();
+
+  // Serial setup
+  Serial.bigin(9600);
+
+  // Button setup
+  pinMode(fuelSolenoidPin, OUTPUT);
+  pinMode(fuelButtonPin, INPUT);
+  pinMode(oxidizerSolenoidPin, OUTPUT);
+  pinMode(oxidizerButtonPin, INPUT);
+  pinMode(armPin, INPUT);
+  pinMode(enableRelayPin, OUTPUT);
+
+  // init pins ?
+  digitalWrite(enableRelayPin, HIGH);
+  digitalWrite(fuelSolenoidPin, HIGH);
+
 }
 
 // SPI interrupt routine
@@ -19,4 +55,45 @@ ISR (SPI_STC_vect)
   SPDR = c+10;
 }  // end of interrupt service routine (ISR) for SPI
 
-void loop () { }
+void loop () {
+    armPinState = digitalRead(armPin);
+  enableRelayState = digitalRead(enableRelayPin);
+  fuelButtonState = digitalRead(fuelButtonPin);
+  fuelOutput = digitalRead(fuelSolenoidPin);
+  if (armPinState == LOW) {
+
+    // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+    if (fuelButtonState == HIGH) {
+      // turn LED on:
+      digitalWrite(fuelSolenoidPin, HIGH);
+    } else {
+      // turn LED off:
+      digitalWrite(fuelSolenoidPin, LOW);
+    }
+    Serial.print("Status armPinState: ");
+    Serial.print(armPinState);
+    Serial.print(" fuelButtonState: ");
+    Serial.print(fuelButtonState);
+    Serial.print(" fuelOutput: ");
+    Serial.print(fuelOutput);
+    Serial.print(" enableRelayState: ");
+    Serial.print(enableRelayState);
+    Serial.print(" enableRelayPin: ");
+    Serial.println(enableRelayPin);
+  } else {
+  //  enableRelayState = 0;
+  //  digitalWrite(enableRelayPin, HIGH);
+    digitalWrite(fuelSolenoidPin, LOW);
+    Serial.print("Status armPinState: ");
+    Serial.print(armPinState);
+    Serial.print(" fuelButtonState: ");
+    Serial.print(fuelButtonState);
+    Serial.print(" fuelOutput: ");
+    Serial.print(fuelOutput);
+    Serial.print(" enableRelayState: ");
+    Serial.print(enableRelayState);
+    Serial.print(" enableRelayPin: ");
+    Serial.println(enableRelayPin);
+  }
+  //udelay(1);
+}
